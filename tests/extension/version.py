90c446ec323d1a4f405b36c3b3630e205fa71040
Maurice Lam <me@mauricelam.com>
2019-05-22 21:20:11 -0700

    Simplify version.py

--- tests/extension/version.py
+++ tests/extension/version.py
@@ -10,29 +10,23 @@ import sys
 from hashbang import command, Argument
 
 
-class VersionArgument(Argument):
-    def __init__(self, *args, version=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.version = version
-
-    def add_argument(self, parser, argname, param, *, partial=False):
-        parser.add_argument(
-            '--version',
-            action='version',
-            version=self.version)
-
-
-class Version:
+class Version(Argument):
     '''
     Demonstrates how an extension can be used to add an extra version argument.
     '''
 
     def __init__(self, version):
         super().__init__()
-        self.versionarg = VersionArgument(version=version)
+        self.version = version
 
     def apply_hashbang_extension(self, cmd):
-        cmd.arguments['version'] = (None, self.versionarg)
+        cmd.arguments['version'] = (None, self)
+
+    def add_argument(self, parser, argname, param, *, partial=False):
+        parser.add_argument(
+            '--version',
+            action='version',
+            version=self.version)
 
 
 @command(Version('0.1.0'))
