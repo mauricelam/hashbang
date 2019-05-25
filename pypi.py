d61148f1ac01cd83fe48dcf43ff99dfa5b32430b
Maurice Lam <me@mauricelam.com>
2019-05-25 00:23:28 -0700

    Tag version "0.0.9"

--- pypi.py
+++ pypi.py
@@ -9,16 +9,34 @@ import tempfile
 
 DIR = Path(__file__).parent.resolve()
 
+VERSION_COMPONENTS = ('major', 'minor', 'patch')
+
+
+def version_bump(current_version, bump='patch'):
+    parts = current_version.split('.')
+    bump_index = VERSION_COMPONENTS.index(bump)
+    parts[bump_index] = str(int(parts[bump_index]) + 1)
+    return '.'.join(parts)
+
 
 @command
-def main(version, *, upload=True):
+def main(bump=None, *, upload=True, version=None, git_status_check=True):
     '''
     Package and upload the built artifact to PyPI
     '''
-    gitstatus = subprocess.check_output(['git', 'status', '--porcelain'])
-    if len(gitstatus) != 0:
-        print('Git directory not clean. Please commit your changes first')
-        sys.exit(2)
+    if git_status_check:
+        gitstatus = subprocess.check_output(['git', 'status', '--porcelain'])
+        if len(gitstatus) != 0:
+            print('Git directory not clean. Please commit your changes first')
+            sys.exit(2)
+    if version is None:
+        if bump is None:
+            print('Please specify a version bump level '
+                  '("major", "minor", "patch")')
+            sys.exit(2)
+        with (DIR/'version.txt').open('r') as f:
+            current_version = f.read()
+            version = version_bump(current_version, bump)
     with (DIR/'version.txt').open('w') as f:
         f.write(version)
     subprocess.check_call(['git', 'commit', '-a', '-m',
