6c8dcd2285f01b33494387bc5b7ddeca6d1ba6b4
Maurice Lam <me@mauricelam.com>
2019-12-29 23:46:09 -0800

    Fix python 3.8

--- tests/extension/kwarg_parser.py
+++ tests/extension/kwarg_parser.py
@@ -3,8 +3,6 @@
 '''
 $ kwarg_parser.py  # returncode=1 stderr=True glob=True
 > Traceback (most recent call last):
->   File "*parser.py", line *, in <module>
->     @command(parser='foobar')
 >   ...
 > RuntimeError: Command property "parser" cannot be set
 '''
